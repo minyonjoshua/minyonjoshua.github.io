01224cf778c0fbce5b5f328a6345f156bdc7d725
Joshua Minyon <minyonjoshua@gmail.com>
2015-06-10 16:57:12 -0400

    Update 2015-06-10T20:55:16.823Z

--- first.js
+++
@@ -1,9 +0,0 @@
-var firstApp = angular.module('firstApp',[]);
-firstApp.controller('FirstController',function($scope){
-	$scope.first = '';
-	$scope.last = '';
-	$scope.heading = 'Message: ';
-	$scope.updateMessage = function(){
-		$scope.message = 'Hello ' + $scope.first +' '+ $scope.last + '!';
-	};
-});
